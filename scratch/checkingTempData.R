70288ace63de7ad383cae5f170ee52dda0d0cefd
Skylar Gering <sgering@g.hmc.edu>
2020-07-17 17:14:16 -0700

    wrote script to clean data

---
+++ scratch/checkingTempData.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+upper_bound = 295
+lower_bound = 275
+path_name = 'Temperature Data'
+
+# Read in CSV Data
+temp_data <- read.csv(file = file.path(path_name, 'temp.csv'),
+                     stringsAsFactors = FALSE)
+
+# Convert time just to year value
+temp_data$Time = round(temp_data$Time/10000)
+
+#split the Ensemble_Model variable into two variables
+temp_data <- separate(temp_data, "Ensemble_Model", 
+                     c("Ensemble", "Model"), "_", 
+                     remove = TRUE)
+
+# Identify data that falls outside the regular range
+bad_data <- filter(temp_data, Temp > upper_bound | Temp < lower_bound)
+bad_models<- unique(bad_data$Model)
+
+# Remove outliers from data
+for(model in bad_models){
+  temp_data <- subset(temp_data, Model != model)   
+}
+
+# Plot to confirm visually that there is no other weird data
+ggplot(temp_data, aes(x=Time, y=Temp, group = Model)) + 
+  geom_line(aes(color = Model)) +
+  facet_grid(cols = vars(Data))
+
+# Write cleaned data to a CSV
+write.csv(temp_data, file.path(path_name, 'cleaned_temp.csv'),
+          row.names = FALSE)
